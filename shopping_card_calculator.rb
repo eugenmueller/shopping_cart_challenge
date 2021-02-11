a1867da7de7cc6d9134efb5730f0442416910251
Eugen <eugen.mllr@gmail.com>
2021-02-11 18:09:33 +0100

    Nice usage of a splat operator.
    Increase readability of the calculate_cart_price Methode.

--- shopping_card_calculator.rb
+++ shopping_card_calculator.rb
@@ -21,16 +21,16 @@ shopping_carts = [
   }
 ]
 
-def calculate_cart_prices(cart)
-  cart[:peaches] * 7 + calculate_apples_price(cart[:apples]) + calculate_grapes_price(cart[:grapes])
+def calculate_cart_prices(grapes: 0, apples: 0, peaches: 0)
+  peaches * 7 + calculate_apples_price(apples) + calculate_grapes_price(grapes)
 end
 
-def calculate_apples_price(count)
-  count > 1 ? (count * 3) - (count * 3).to_f / 100.00 * 20 : count * 3
+def calculate_apples_price(apples)
+  apples > 1 ? (apples * 3) - (apples * 3).to_f / 100.00 * 20 : apples * 3
 end
 
-def calculate_grapes_price(count)
-  count > 1 && count.odd? ? (count - 1) * 5 : count * 5
+def calculate_grapes_price(grapes)
+  grapes > 1 && grapes.odd? ? (grapes - 1) * 5 : grapes * 5
 end
 
-shopping_carts.each { |cart| puts calculate_cart_prices(cart) }
+shopping_carts.each { |cart| puts calculate_cart_prices(**cart) }
